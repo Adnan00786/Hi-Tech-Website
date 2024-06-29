d9f2656ce8421926da5d2ae839c36d418ccdbd60
Adnan <adnanali23189@gmail.com>
2024-06-29 19:50:27 +0530

    About Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Topbar from "@/components/Topbar";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Topbar/>
       <Navbar/>
       <Hero/>
+      <About/>
     </>
   );
 }
